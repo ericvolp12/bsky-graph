218bfad48821ef78077433abe9150c3d5be8b662
Eric Volpert <ericvolp12@gmail.com>
2023-04-29 14:27:35 -0700

    Updated at feature

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,6 +1,8 @@
 import React, { FC, useEffect, useRef } from "react";
 import { useSearchParams } from "react-router-dom";
 import { MultiDirectedGraph } from "graphology";
+import * as dayjs from "dayjs";
+import * as relativeTime from "dayjs/plugin/relativeTime";
 import getNodeProgramImage from "sigma/rendering/webgl/programs/node.image";
 import {
   SigmaContainer,
@@ -11,6 +13,8 @@ import {
 import "@react-sigma/core/lib/react-sigma.min.css";
 
 import { CustomSearch } from "./CustomSearch";
+
+dayjs.extend(relativeTime);
 
 // Hook
 function usePrevious<T>(value: T): T {
@@ -522,16 +526,22 @@ const SocialGraph: React.FC<{}> = () => {
             >
               jaz
             </a>
-            {" 🏳️‍⚧️ "}
+            {" 🏳️‍⚧️"}
+          </span>
+          <span className="footer-text text-xs">
+            {" | "}
+            {dayjs().to(graph?.getAttribute("lastUpdated"))}{" "}
+            <img src="/update-icon.svg" className="inline-block h-4 w-4" />
+            {" | "}
             <a
               href="https://github.com/ericvolp12/bsky-experiments"
               target="_blank"
             >
-              <img src="/github.svg" className="inline-block h-4 w-4" />
+              <img
+                src="/github.svg"
+                className="inline-block h-3.5 w-4 mb-0.5"
+              />
             </a>
-          </span>
-          <span className="footer-text text-xs">
-            {" | "}Updated: {graph?.getAttribute("lastUpdated")}
           </span>
         </div>
       </footer>
