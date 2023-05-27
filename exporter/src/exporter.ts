5aa18b8276c1754e2293cfbf84214b9d44d83537
Jaz <ericvolp12@gmail.com>
2023-05-27 14:23:39 -0700

    Updated barycenter computation, drop edges of weight <=2

--- exporter/src/exporter.ts
+++ exporter/src/exporter.ts
@@ -143,7 +143,7 @@ async function fetchGraph() {
       return;
     }
     const parsedWeight = parseInt(weight);
-    if (parsedWeight > 1 && source !== target) {
+    if (parsedWeight > 2 && source !== target) {
       const sourceNode = { did: source, handle: sourceHandle };
       if (!nodesMap.has(source) && source !== "" && sourceHandle !== "") {
         nodesMap.set(source, sourceNode);
@@ -350,7 +350,7 @@ fetchGraph().then((graphData: { edges: Edge[]; nodes: Node[] }) => {
   log("Assigning community partitions...");
   // To directly assign communities as a node attribute
   louvain.assign(graph, {
-    resolution: 1.08,
+    resolution: 1.05,
   });
   log("Done assigning community partitions");
 
@@ -406,31 +406,84 @@ fetchGraph().then((graphData: { edges: Edge[]; nodes: Node[] }) => {
   log("Done truncating node position assignments");
 
   graph.forEachNode((_, atts) => {
-    if (!atts.community) return;
+    if (atts.community === undefined || atts.community === null) return;
     const cluster = communityClusters[atts.community];
     if (cluster === undefined) return;
-    // store cluster's nodes positions to calculate cluster label position
     cluster.positions.push({ x: atts.x, y: atts.y });
   });
 
-  // calculate the cluster's nodes barycenter to use this as cluster label position
+  // Filter positions that are 2 standard deviations away from the mean and compute the barycenter of each cluster
   for (const community in communityClusters) {
+    let x_positions = communityClusters[community].positions.map((p) => p.x);
+    let y_positions = communityClusters[community].positions.map((p) => p.y);
+
+    log(`Processing community ${communityClusters[community].label}...`);
+
+    if (x_positions.length === 0 || y_positions.length === 0) {
+      log(`Skipping community ${communityClusters[community].label}...`);
+      continue; // Skip this community if it has no positions
+    }
+
+    const mean_x =
+      x_positions.reduce((acc, x) => acc + x, 0) / x_positions.length;
+    const mean_y =
+      y_positions.reduce((acc, y) => acc + y, 0) / y_positions.length;
+
+    const std_x = Math.sqrt(
+      x_positions
+        .map((x) => Math.pow(x - mean_x, 2))
+        .reduce((a, b) => a + b, 0) / x_positions.length
+    );
+    const std_y = Math.sqrt(
+      y_positions
+        .map((y) => Math.pow(y - mean_y, 2))
+        .reduce((a, b) => a + b, 0) / y_positions.length
+    );
+
+    log(
+      `Community ${communityClusters[community].label} mean: (${mean_x}, ${mean_y}) std: (${std_x}, ${std_y})`
+    );
+
+    log(
+      `Community ${communityClusters[community].label} positions: ${communityClusters[community].positions.length}`
+    );
+
+    const filtered_positions = communityClusters[community].positions.filter(
+      (p) =>
+        Math.abs(p.x - mean_x) <= 2 * std_x &&
+        Math.abs(p.y - mean_y) <= 2 * std_y
+    );
+
+    log(
+      `Community ${communityClusters[community].label} filtered positions: ${filtered_positions.length}`
+    );
+
+    if (filtered_positions.length === 0) {
+      log(`Skipping community ${communityClusters[community].label}...`);
+      continue; // Skip this community if there are no positions within 2 standard deviations
+    }
+
     communityClusters[community].x = parseFloat(
       (
-        communityClusters[community].positions.reduce(
-          (acc, p) => acc + p.x,
-          0
-        ) / communityClusters[community].positions.length
+        filtered_positions.reduce((acc, p) => acc + p.x, 0) /
+        filtered_positions.length
       ).toFixed(2)
     );
     communityClusters[community].y = parseFloat(
       (
-        communityClusters[community].positions.reduce(
-          (acc, p) => acc + p.y,
-          0
-        ) / communityClusters[community].positions.length
+        filtered_positions.reduce((acc, p) => acc + p.y, 0) /
+        filtered_positions.length
       ).toFixed(2)
     );
+
+    log(
+      `Community ${communityClusters[community].label} barycenter: (${communityClusters[community].x}, ${communityClusters[community].y})`
+    );
+  }
+
+  // Strip the positions from the cluster objects
+  for (const community in communityClusters) {
+    communityClusters[community].positions = [];
   }
 
   graph.setAttribute("clusters", communityClusters);
