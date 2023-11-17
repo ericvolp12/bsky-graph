cefa07ce625ef66bf750e425e2c916106317221b
Jaz <ericvolp12@gmail.com>
2023-11-17 11:57:30 -0800

    Move preview to just above buttons

--- src/components/repocleanup/RepoCleanup.tsx
+++ src/components/repocleanup/RepoCleanup.tsx
@@ -326,6 +326,14 @@ const RepoCleanup: FC<{}> = () => {
                                 />
                                 <span> days</span>
                             </div>
+                            <div className="mt-3 text-sm w-full leading-6 text-gray-500 text-center">
+                                <p className="text-xl text-gray-700 mb-2">Things to Keep in Mind</p>
+                                <p>Bluesky has creation rate limits of ~1,300 posts/likes/reposts per hour and ~11,000 per day</p>
+                                <p>Deleting content counts as 1/3 of a post/like/repost</p>
+                                <p>This tool deletes up to 4,000 records per hour and 30,000 per day</p>
+                                <p>You should still be able to like/post/repost normally unless you're <i>very</i> active</p>
+                                <p>If you start getting "Rate Limit" related errors, you can come back and cancel your job</p>
+                            </div>
                             {resp && (
                                 <div className="flex gap-2 justify-center">
                                     <div className="w-auto flex">
@@ -337,14 +345,6 @@ const RepoCleanup: FC<{}> = () => {
                                     </div>
                                 </div>
                             )}
-                            <div className="mt-3 text-sm w-full leading-6 text-gray-500 text-center">
-                                <p className="text-xl text-gray-700 mb-2">Things to Keep in Mind</p>
-                                <p>Bluesky has creation rate limits of ~1,300 posts/likes/reposts per hour and ~11,000 per day</p>
-                                <p>Deleting content counts as 1/3 of a post/like/repost</p>
-                                <p>This tool deletes up to 4,000 records per hour and 30,000 per day</p>
-                                <p>You should still be able to like/post/repost normally unless you're <i>very</i> active</p>
-                                <p>If you start getting "Rate Limit" related errors, you can come back and cancel your job</p>
-                            </div>
                             <div className="mt-4 sm:ml-4 sm:flex-shrink-0 flex">
                                 <button
                                     onClick={async (e) => {
